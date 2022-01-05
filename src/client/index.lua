b2bb5cb90bd52caebfba796e7aa2f07ed892b857
mrgreenh <cconte@roblox.com>
2022-01-04 16:49:13 -0800

    refactorings I did two weeks ago and hope made sense

--- src/client/index.lua
+++ src/client/index.lua
@@ -12,19 +12,8 @@ return function()
 			Transparency = 1,
 			CanCollide = false,
 		}),
-		UIPart = Roact.createElement("Part", {
-			Size = Vector3.new(21, 8, 0.1),
-			Position = Vector3.new(-2.5, 4.5, -10),
-			Orientation = Vector3.new(0, 180, 0),
-		}, {
-			GuiSurface = Roact.createElement("SurfaceGui", {
-				CanvasSize = Vector2.new(800, 400),
-				SizingMode = "FixedSize",
-			}, {
-				Counter = Roact.createElement(Counter),
-			}),
-		}),
-		Tree = Roact.createElement(Forest, {
+		Counter = Roact.createElement(Counter),
+		Forest = Roact.createElement(Forest, {
 			treeCount = 20,
 		}),
 	})
--- src/shared/Components/Counter.lua
+++ src/shared/Components/Counter.lua
@@ -10,25 +10,36 @@ function Counter:init()
 end
 
 function Counter:render()
-	return Roact.createElement("Frame", {
-		Size = UDim2.new(1, 0, 1, 0),
-		BackgroundColor3 = Color3.new(0, 0, 1),
+	return Roact.createElement("Part", {
+		Size = Vector3.new(21, 8, 0.1),
+		Position = Vector3.new(-2.5, 4.5, -10),
+		Orientation = Vector3.new(0, 180, 0),
 	}, {
-		Button = Roact.createElement("TextButton", {
-			Size = UDim2.new(0.5, 0, 1, 0),
-			Text = "Clickme",
-			[Roact.Event.Activated] = function()
-				self:setState(function()
-					return {
-						count = self.state.count + 1,
-					}
-				end)
-			end,
-		}),
-		Value = Roact.createElement("TextLabel", {
-			Size = UDim2.new(0.5, 0, 1, 0),
-			Position = UDim2.new(0.5, 0, 0, 0),
-			Text = tostring(self.state.count),
+		GuiSurface = Roact.createElement("SurfaceGui", {
+			CanvasSize = Vector2.new(800, 400),
+			SizingMode = "FixedSize",
+		}, {
+			Roact.createElement("Frame", {
+				Size = UDim2.new(1, 0, 1, 0),
+				BackgroundColor3 = Color3.new(0, 0, 1),
+			}, {
+				Button = Roact.createElement("TextButton", {
+					Size = UDim2.new(0.5, 0, 1, 0),
+					Text = "Clickme",
+					[Roact.Event.Activated] = function()
+						self:setState(function()
+							return {
+								count = self.state.count + 1,
+							}
+						end)
+					end,
+				}),
+				Value = Roact.createElement("TextLabel", {
+					Size = UDim2.new(0.5, 0, 1, 0),
+					Position = UDim2.new(0.5, 0, 0, 0),
+					Text = tostring(self.state.count),
+				}),
+			}),
 		}),
 	})
 end
